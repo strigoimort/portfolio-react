5decfc0da5bc809ed269ef323d1a428743065a7a
Petrik Siano <petriksiano24@gmail.com>
2026-04-20 13:21:08 +0700

    import speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Portfolio from "./components/Portfolio";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
@@ -20,8 +21,9 @@ function App() {
       <Footer />
       <ScrollToTop />
 
-      {/* taruh di paling bawah */}
       <Analytics />
+      <SpeedInsights />
+
     </div>
   );
 }
